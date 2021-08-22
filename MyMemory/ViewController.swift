3e11b645652c602c47e5d505df4e529514a33c98
hakenLim <haken1984@gmail.com>
2021-08-22 17:29:26 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by Hak Hyun Lim on 2021/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
